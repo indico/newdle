74d847d08f84d68e5a14df731eb95d7ac94b4db8
Panagiota Poulopoulou <panagiota.poulopoulou@cern.ch>
2019-07-24 15:43:38 +0200

    Add Navigator Component

--- newdle/client/src/App.js
+++ newdle/client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import {Header, Modal} from 'semantic-ui-react';
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import {Modal} from 'semantic-ui-react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './Home';
 import CreateNewdle from './CreateNewdle';
-import UserMenu from './UserMenu';
 import {isRefreshingToken} from './selectors';
 import {useAuthentication} from './auth';
-import styles from './App.module.scss';
+import TopHeader from './components/TopHeader';
+import './App.module.scss';
 
 export default function App() {
   const refreshing = useSelector(isRefreshingToken);
@@ -16,19 +16,12 @@ export default function App() {
   return (
     <Router>
       <main>
-        <header className={styles.header}>
-          <Header as="h1" className={styles.title}>
-            <Link to="/">newdle</Link>
-          </Header>
-          <UserMenu />
-        </header>
-        <section>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/new" component={CreateNewdle} />
-            <Route render={() => 'This page does not exist'} />
-          </Switch>
-        </section>
+        <TopHeader />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/new" component={CreateNewdle} />
+          <Route render={() => 'This page does not exist'} />
+        </Switch>
         {refreshing && (
           <Modal
             open
