a05dc328ede46f390a7437109301bfa1fc8deef0
Pedro Ferreira <pedro.ferreira@cern.ch>
2019-08-01 17:57:45 +0200

    Add POST method to create newdles

--- newdle/schemas.py
+++ newdle/schemas.py
@@ -1,6 +1,8 @@
 from marshmallow import fields, post_dump
+from pytz import common_timezones_set
 
 from .core.marshmallow import mm
+from .core.util import DATETIME_FORMAT
 
 
 class UserSchema(mm.Schema):
@@ -19,3 +21,28 @@ class UserSearchResultSchema(UserSchema):
         if many:
             data = sorted(data, key=lambda x: x['name'].lower())
         return data
+
+
+class ParticipantSchema(mm.Schema):
+    name = fields.String(required=True)
+    email = fields.String()
+    auth_uid = fields.String()
+
+
+class SlotSchema(mm.Schema):
+    start = fields.DateTime(required=True, format=DATETIME_FORMAT)
+    end = fields.DateTime(required=True, format=DATETIME_FORMAT)
+
+
+class NewNewdleSchema(mm.Schema):
+    title = fields.String(validate=lambda x: len(x) >= 3, required=True)
+    duration = fields.Int(required=True, validate=lambda x: x % 15 == 0)
+    timezone = fields.String(
+        validate=lambda x: x in common_timezones_set, required=True
+    )
+    time_slots = fields.List(fields.Nested(SlotSchema), validate=bool, required=True)
+    participants = fields.List(fields.Nested(ParticipantSchema), missing=[])
+
+
+class NewdleSchema(NewNewdleSchema):
+    id = fields.Integer()
