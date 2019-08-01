a1ac3caba43780f145c9d7a17d7f30557199d68f
Natalia Juszka <natalia.karina.juszka@cern.ch>
2019-08-01 16:26:52 +0200

    Move Timeline and Participant to /components

--- newdle/client/src/TimelineExample.js
+++ newdle/client/src/TimelineExample.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Timeline from './Timeline';
+import Timeline from './components/Timeline';
 
 export default function TimelineExample() {
   const availability = [
