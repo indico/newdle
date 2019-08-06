8b2b6d10e5b1f0b350ab03c551f4db31457ba6db
Natalia Juszka <natalia.karina.juszka@cern.ch>
2019-08-06 10:56:14 +0200

    Use participants from step 1 in step 2
    Generate random participant availabilities

--- newdle/client/src/TimelineExample.js
+++ newdle/client/src/TimelineExample.js
@@ -1,59 +1,28 @@
 import React from 'react';
 import Timeline from './components/Timeline';
+import moment from 'moment';
 
-export default function TimelineExample() {
-  const availability = [
-    {
-      participant: {
-        id: 1,
-        name: 'First Person',
-        initials: 'F P',
-        email: 'f.person@mail.com',
-      },
-      busySlots: [
-        {
-          startTime: '5:00',
-          endTime: '9:00',
-        },
-        {
-          startTime: '16:30',
-          endTime: '22:00',
-        },
-      ],
-    },
-    {
-      participant: {
-        id: 2,
-        name: 'Second Person',
-        initials: 'S P',
-        email: 's.person@mail.com',
-      },
-      busySlots: [
-        {
-          startTime: '14:00',
-          endTime: '15:00',
-        },
-        {
-          startTime: '15:30',
-          endTime: '19:00',
-        },
-      ],
-    },
-    {
-      participant: {
-        id: 3,
-        name: 'Third Person',
-        initials: 'T P',
-        email: 't.person@mail.com',
-      },
-      busySlots: [
-        {
-          startTime: '21:00',
-          endTime: '23:30',
-        },
-      ],
-    },
-  ];
-
+export default function TimelineExample({participants}) {
+  const availability = generateParticipantAvailability(participants);
   return <Timeline date="2019-07-29" availability={availability} />;
+}
+
+function generateParticipantAvailability(participants) {
+  let availability = [];
+  participants.forEach(participant => {
+    const start = randomNumber(6, 22);
+    const end = randomNumber(start, 23);
+    const busySlots = [
+      {
+        startTime: moment(start, 'k').format('k:mm'),
+        endTime: moment(end, 'k').format('k:mm'),
+      },
+    ];
+    availability.push({participant, busySlots});
+  });
+  return availability;
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
