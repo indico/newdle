b0984b0faabe10aae9195ab18844352cb58b9629
Natalia Juszka <natalia.karina.juszka@cern.ch>
2019-09-26 12:14:44 +0200

    Use util/date functions

--- newdle/client/src/components/AnswerOption.js
+++ newdle/client/src/components/AnswerOption.js
@@ -1,14 +1,16 @@
 import {Checkbox} from 'semantic-ui-react';
-import moment from 'moment';
+import {serializeDate, toMoment} from '../util/date';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './Answer.module.scss';
 
 export default function AnswerOption({startTime, endTime}) {
+  const start = serializeDate(toMoment(startTime, 'H:mm'), 'H:mm');
+  const end = serializeDate(toMoment(endTime, 'H:mm'), 'H:mm');
   return (
     <div className={styles['option']}>
       <div className={styles['times']}>
-        {moment(startTime, 'H:mm').format('H:mm')} - {moment(endTime, 'H:mm').format('H:mm')}
+        {start} - {end}
       </div>
       <Checkbox />
     </div>
