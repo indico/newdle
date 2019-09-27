d90cd779169f937c331e39916320c932a7884998
Adrian Moennich <adrian.moennich@cern.ch>
2019-09-27 17:09:18 +0200

    Make timezone selector dropdown lazy
    It takes quite long to render, and most people won't change its value,
    so now we just render some simple HTML and only turn it into a real
    dropdown when clicking it.

--- newdle/client/src/components/TimezonePicker.js
+++ newdle/client/src/components/TimezonePicker.js
@@ -1,37 +1,34 @@
 import React, {useMemo} from 'react';
-import {Dropdown} from 'semantic-ui-react';
 import {useDispatch, useSelector} from 'react-redux';
 import {getTimezone} from '../selectors';
 import {setTimezone} from '../actions';
-
 import {commonTimezones} from '../util/timezones';
+import LazyDropdown from './LazyDropdown';
 import styles from './TimezonePicker.module.scss';
 
-export default function TimezonePicker() {
+export default React.memo(function TimezonePicker() {
   const dispatch = useDispatch();
   const timezone = useSelector(getTimezone);
   const options = useMemo(
     () =>
-      commonTimezones.map(({name, caption}) => {
-        return {
-          key: name,
-          value: name,
-          text: name,
-          description: caption,
-          active: name === timezone,
-        };
-      }),
-    [timezone]
+      commonTimezones.map(({name, caption}) => ({
+        key: name,
+        value: name,
+        text: name,
+        description: caption,
+      })),
+    []
   );
   return (
     <div>
       <span>Timezone</span>
-      <Dropdown
-        className={styles['dropdown']}
+      <LazyDropdown
+        className={styles.dropdown}
         options={options}
         search
         selection
         selectOnBlur={false}
+        selectOnNavigation={false}
         value={timezone}
         onChange={(_, {value}) => {
           dispatch(setTimezone(value));
@@ -39,4 +36,4 @@ export default function TimezonePicker() {
       />
     </div>
   );
-}
+});
