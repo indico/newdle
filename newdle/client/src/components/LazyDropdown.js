2c88ca2e6105e6129c3c445cb7030728bab26961
Adrian Moennich <adrian.moennich@cern.ch>
2019-09-30 16:38:47 +0200

    Simplify LazyDropdown code a lot

--- newdle/client/src/components/LazyDropdown.js
+++ newdle/client/src/components/LazyDropdown.js
@@ -4,44 +4,12 @@ import {Dropdown} from 'semantic-ui-react';
 export default function LazyDropdown(props) {
   const [activated, setActivated] = useState(false);
 
-  const handleActivate = () => {
-    setActivated(true);
-  };
-
   if (!activated) {
-    const activeText = props.value ? props.options.find(x => x.value === props.value).text : '';
-    return (
-      /* eslint-disable jsx-a11y/role-has-required-aria-props */
-      <div
-        role="combobox"
-        aria-expanded="false"
-        className={`ui search selection dropdown ${props.className}`}
-        onClick={handleActivate}
-      >
-        <input
-          aria-autocomplete="list"
-          autoComplete="off"
-          className="search"
-          tabIndex="0"
-          type="text"
-          onFocus={handleActivate}
-          onClick={handleActivate}
-        />
-        <div className="text" role="alert" aria-live="polite" aria-atomic="true">
-          {activeText}
-        </div>
-        <i aria-hidden="true" className="dropdown icon" />
-      </div>
-    );
+    const activeOptions = props.options.filter(x => x.value === props.value);
+    return <Dropdown {...props} onOpen={() => setActivated(true)} options={activeOptions} />;
   }
 
-  return (
-    <Dropdown
-      {...props}
-      defaultOpen
-      openOnFocus={false} /* otherwise it stays open after the first click on an option */
-    />
-  );
+  return <Dropdown {...props} />;
 }
 
 LazyDropdown.propTypes = Dropdown.propTypes;
