03c0563129ee3194c2f856ba167b0b55c9c59a8a
Adrian Moennich <adrian.moennich@cern.ch>
2019-09-30 16:00:35 +0200

    Indicate if busy times are still loading

--- newdle/client/src/components/TimelineRow.js
+++ newdle/client/src/components/TimelineRow.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import UserAvatar from './UserAvatar';
+import PendingSlot from './PendingSlot';
 import Slot from './Slot';
 import styles from './Timeline.module.scss';
 
-export default function TimelineRow({participant, busySlots}) {
+export default function TimelineRow({participant, busySlots, busySlotsLoading}) {
   return (
     <div className={styles['timeline-row']}>
       <span className={styles['timeline-row-label']}>
         <UserAvatar user={participant} className={styles['avatar']} size={30} withLabel />
       </span>
       <div className={styles['timeline-busy']}>
-        {busySlots.map(slot => (
-          <Slot {...slot} />
-        ))}
+        {busySlotsLoading ? <PendingSlot /> : busySlots.map(slot => <Slot {...slot} />)}
       </div>
     </div>
   );
