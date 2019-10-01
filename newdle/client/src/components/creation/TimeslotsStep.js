e90bcf98975dfa5e2bcc28170a26eb3e6410dc33
Adrian Moennich <adrian.moennich@cern.ch>
2019-10-01 15:54:42 +0200

    Split CreateNewdle.js

---
+++ newdle/client/src/components/creation/TimeslotsStep.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Button, Grid, Icon} from 'semantic-ui-react';
+import {getFullTimeslots, getMeetingParticipants} from '../../selectors';
+import Calendar from '../Calendar';
+import Availability from '../Availability';
+import {setStep} from '../../actions';
+import styles from './creation.module.scss';
+
+export default function TimeslotsStep() {
+  const dispatch = useDispatch();
+  const participants = useSelector(getMeetingParticipants);
+  const timeslots = useSelector(getFullTimeslots);
+  return (
+    <div>
+      <Grid container>
+        <Grid.Row columns={2}>
+          <Grid.Column width={5}>
+            <Calendar />
+          </Grid.Column>
+          <Grid.Column width={11}>
+            <Availability participants={participants} />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <div className={styles['button-row']}>
+            <Button color="violet" icon labelPosition="left" onClick={() => dispatch(setStep(1))}>
+              Back
+              <Icon name="angle left" />
+            </Button>
+            <Button
+              color="violet"
+              icon
+              labelPosition="right"
+              disabled={!timeslots.length}
+              onClick={() => dispatch(setStep(3))}
+            >
+              Next step
+              <Icon name="angle right" />
+            </Button>
+          </div>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
