8dd4252dde5f3a27734b288f718609dc757cd591
Adrian Moennich <adrian.moennich@cern.ch>
2019-10-01 16:06:16 +0200

    Move success page to creation

--- newdle/client/src/components/creation/index.js
+++ newdle/client/src/components/creation/index.js
@@ -1,2 +1,3 @@
 export {default as CreationPage} from './CreationPage';
+export {default as CreationSuccessPage} from './CreationSuccessPage';
 export {default as CreationHeader} from './CreationHeader';
