90eb148150e52aa2f514be89dd18085e3c0f3908
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2019-10-14 15:48:58 +0200

    Decrease the size of icon in Option

--- newdle/client/src/components/answer/Option.js
+++ newdle/client/src/components/answer/Option.js
@@ -1,18 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import {Icon} from 'semantic-ui-react';
 import {serializeDate, toMoment} from '../../util/date';
-import PropTypes from 'prop-types';
-import React from 'react';
 import styles from './answer.module.scss';
 
 export default function Option({startTime, endTime, icon, onClick, className}) {
   const start = serializeDate(toMoment(startTime, 'H:mm'), 'H:mm');
   const end = serializeDate(toMoment(endTime, 'H:mm'), 'H:mm');
+
   return (
     <div className={`${styles.option} ${className}`} onClick={onClick}>
       <span className={styles.times}>
         {start} - {end}
       </span>
-      <Icon name={icon} size="large" />
+      <Icon name={icon} />
     </div>
   );
 }
