b6f740da6297c19cc1908630966371f6f9c2a23c
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2019-10-14 11:14:34 +0200

    Use DayCarousel on the answer page

--- newdle/client/src/components/DayCarousel.js
+++ newdle/client/src/components/DayCarousel.js
@@ -5,17 +5,13 @@ import {Icon} from 'semantic-ui-react';
 import styles from './DayCarousel.module.scss';
 
 export default function DayCarousel({items, numberOfVisible, start, step, renderItem, changeItem}) {
-  const [startIndex, setStartIndex] = useState(start);
-
   const next = () => {
-    const newIndex = startIndex + step > items.length ? items.length - 1 : startIndex + step;
-    setStartIndex(newIndex);
+    const newIndex = (start + step) % items.length;
     changeItem(items[newIndex]);
   };
 
   const prev = () => {
-    const newIndex = startIndex - step < 0 ? 0 : startIndex - step;
-    setStartIndex(newIndex);
+    const newIndex = Math.max(start - step, 0);
     changeItem(items[newIndex]);
   };
 
@@ -24,7 +20,7 @@ export default function DayCarousel({items, numberOfVisible, start, step, render
       {start !== 0 && (
         <Icon size="big" name="angle left" onClick={prev} className={styles['prev-icon']} />
       )}
-      {start + numberOfVisible <= items.length - 1 && (
+      {start + numberOfVisible <= items.length + 1 && (
         <Icon size="big" name="angle right" onClick={next} className={styles['next-icon']} />
       )}
       {items.slice(start, start + numberOfVisible).map(renderItem)}
