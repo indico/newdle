35468162334db0c0f4930253c5a3a0ea13d0f336
Adrian Moennich <adrian.moennich@cern.ch>
2019-10-24 09:38:44 +0200

    Set flask env vars in dockerfile
    That way running the container e.g. with `flask shell` will also work

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'upgrading database to latest revision'
-FLASK_ENV=production FLASK_APP=newdle.wsgi flask db upgrade
+flask db upgrade
 
 echo 'running uwsgi...'
 exec uwsgi --ini /uwsgi.ini
