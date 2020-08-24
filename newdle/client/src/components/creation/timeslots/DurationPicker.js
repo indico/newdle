929db76280eb9c79cc52c2ac2ac819189071797b
Lea Tschiersch <lea.tschiersch@cern.ch>
2020-08-24 12:38:47 +0200

    Add custom duration picker

--- newdle/client/src/components/creation/timeslots/DurationPicker.js
+++ newdle/client/src/components/creation/timeslots/DurationPicker.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import TimePicker from 'rc-time-picker';
 import {Dropdown} from 'semantic-ui-react';
 import {setDuration} from '../../../actions';
 import {getDuration} from '../../../selectors';
+import {toMoment} from '../../../util/date';
+
+import 'rc-time-picker/assets/index.css';
+import styles from './DurationPicker.module.scss';
+
+const MAX_DURATION_HOURS = 8;
 
 function _minutesToHM(minutes) {
   const hours = Math.floor(minutes / 60);
@@ -27,19 +34,59 @@ function getDurationOptions(min, max, interval) {
 }
 
 export default function DurationPicker({minDuration, maxDuration, interval}) {
+  const [showCustomDuration, setShowCustomDuration] = useState(false);
+  const [durationPickerOpen, setDurationPickerOpen] = useState(false);
   const duration = useSelector(getDuration);
   const durationOptions = getDurationOptions(minDuration, maxDuration, interval);
+  if (!durationOptions.map(option => option.key).includes(duration)) {
+    durationOptions.push({key: duration, value: duration, text: _minutesToHM(duration)});
+  }
+  durationOptions.push({key: 'custom', value: null, text: 'Custom'});
   const dispatch = useDispatch();
+
+  const disabledHours = () => _.range(MAX_DURATION_HOURS + 1, 24, 1);
+  const disabledMinutes = h => (h === 0 ? [0] : []);
 
   return (
     <div>
-      <span>Meeting time</span>
-      <Dropdown
-        options={durationOptions}
-        selection
-        value={duration}
-        onChange={(_, {value}) => dispatch(setDuration(value))}
-      />
+      <span>Duration</span>
+      {showCustomDuration ? (
+        <TimePicker
+          className={styles['duration-picker']}
+          popupClassName={styles['duration-input']}
+          open={durationPickerOpen}
+          onOpen={() => setDurationPickerOpen(true)}
+          onClose={() => setDurationPickerOpen(false)}
+          showSecond={false}
+          value={toMoment('00:00', 'HH:mm').add(duration, 'm')}
+          format="H[h] mm[min]"
+          onChange={time => {
+            if (time !== null) {
+              const value = time.hours() * 60 + time.minutes();
+              dispatch(setDuration(value));
+            }
+          }}
+          allowEmpty={false}
+          minuteStep={15}
+          disabledHours={disabledHours}
+          disabledMinutes={disabledMinutes}
+          hideDisabledOptions
+        />
+      ) : (
+        <Dropdown
+          options={durationOptions}
+          selection
+          value={duration}
+          onChange={(_, {value}) => {
+            if (value !== null) {
+              dispatch(setDuration(value));
+            } else {
+              setShowCustomDuration(true);
+              setDurationPickerOpen(true);
+            }
+          }}
+        />
+      )}
     </div>
   );
 }
