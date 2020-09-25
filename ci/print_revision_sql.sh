5d0c121df70180337748cbb87d756050225fdcca
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-09-25 08:26:22 +0200

    Address MR comments

--- ci/print_revision_sql.sh
+++ ci/print_revision_sql.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-# Get the latest migration from the file received as a parameter
-# and print it with escaped newlines
-set -e;
+# Get all the SQL queries from the first migration file to the last, in order
+# and print them with escaped newlines
+set -e
 
 
 migration_file=$1
+latest_migration=$2
 
-if [ ! -z ${migration_file} ]; then 
-    down_revision=$(grep -Po "(?<=^down_revision = ').*(?=')" $migration_file );
-    revision=$(grep -Po "(?<=^revision = ').*(?=')" $migration_file );
-    sql_command=$(flask db upgrade --sql ${down_revision}:${revision});
+if [ ! -z ${migration_file} ] && [ ! -z ${latest_migration} ]; then 
+    down_revision=$(grep -Po "(?<=^down_revision = ').*(?=')" $migration_file )
+    revision=$(grep -Po "(?<=^revision = ').*(?=')" $latest_migration )
+    sql_command=$(flask db upgrade --sql ${down_revision}:${revision})
     sql_command="${sql_command//'%'/'%25'}"
     sql_command="${sql_command//$'\n'/'%0A'}"
     sql_command="${sql_command//$'\r'/'%0D'}" 
-    echo $sql_command;
+    echo $sql_command
 else 
     exit
 fi
