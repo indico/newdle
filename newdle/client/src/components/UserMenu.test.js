2f46ab3ee70c0595094ee77ff8e5bab2c2b162fe
nidble <abertulla@gmail.com>
2020-10-02 17:30:35 +0200

    Add i18n for UserMenu (#297)

--- newdle/client/src/components/UserMenu.test.js
+++ newdle/client/src/components/UserMenu.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import {i18n} from '@lingui/core';
+import {I18nProvider} from '@lingui/react';
 import {mount} from 'enzyme';
 import {Icon} from 'semantic-ui-react';
 import {configureMockStore} from '@jedmao/redux-mock-store';
@@ -22,11 +24,13 @@ describe('<UserMenu />', () => {
   it('renders an icon for anonymous users', () => {
     const store = mockAnonStore();
     const component = mount(
-      <Router>
-        <Provider store={store}>
-          <UserMenu />
-        </Provider>
-      </Router>
+      <I18nProvider i18n={i18n}>
+        <Router>
+          <Provider store={store}>
+            <UserMenu />
+          </Provider>
+        </Router>
+      </I18nProvider>
     );
     expect(component.contains(Icon)).toBe(true);
     expect(component.exists('.user-gravatar')).toBe(false);
@@ -36,11 +40,13 @@ describe('<UserMenu />', () => {
   it('renders a gravatar for logged-in users', () => {
     const store = mockUserStore();
     const component = mount(
-      <Router>
-        <Provider store={store}>
-          <UserMenu />
-        </Provider>
-      </Router>
+      <I18nProvider i18n={i18n}>
+        <Router>
+          <Provider store={store}>
+            <UserMenu />
+          </Provider>
+        </Router>
+      </I18nProvider>
     );
     expect(component.exists(Icon)).toBe(false);
     expect(component.exists('.user-gravatar')).toBe(true);
