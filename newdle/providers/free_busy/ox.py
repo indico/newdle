d6ff4a8ff294349ce8802565d3fe944b0497703f
Javier Ferrer <javier.ferrer@cern.ch>
2021-05-07 10:43:44 +0200

    Apply suggestions from code review
    Co-authored-by: Adrian <adrian@planetcoding.net>

--- newdle/providers/free_busy/ox.py
+++ newdle/providers/free_busy/ox.py
@@ -9,14 +9,12 @@ from requests.models import HTTPBasicAuth
 from ...core.util import find_overlap
 
 
-MAX_WEEKS_TO_FETCH = 28
-
-
 def fetch_free_busy(date, tz, uid, email):
     ox_url = current_app.config['OX_PROVIDER_URL']
     ox_username = current_app.config['OX_PROVIDER_USERNAME']
     ox_password = current_app.config['OX_PROVIDER_PASSWORD']
     ox_context_id = current_app.config['OX_PROVIDER_CONTEXT_ID']
+    ox_max_weeks = current_app.config.get('OX_PROVIDER_MAX_WEEKS', 4)
 
     if not ox_url or not ox_username or not ox_password or not ox_context_id:
         raise RuntimeError('OX provider not configured!')
@@ -26,7 +24,7 @@ def fetch_free_busy(date, tz, uid, email):
     # see how far into the future we have to fetch data
     weeks_to_fetch = math.ceil((date - datetime.now().date()).days / 7) or 1
 
-    if weeks_to_fetch > MAX_WEEKS_TO_FETCH:
+    if weeks_to_fetch > ox_max_weeks:
         return []
 
     params = {
@@ -40,13 +38,12 @@ def fetch_free_busy(date, tz, uid, email):
         ox_url, params=params, auth=HTTPBasicAuth(ox_username, ox_password)
     )
 
-    if resp.status_code != 200:
+    if not resp.ok:
         return []
 
     busy_slots = []
 
-    for b_line in resp.iter_lines():
-        line = b_line.decode()
+    for line in resp.text.splitlines():
         # this includes BUSY, BUSY-UNAVAILABLE and BUSY-TENTATIVE
         if line.startswith('FREEBUSY;FBTYPE=BUSY'):
             try:
@@ -54,7 +51,6 @@ def fetch_free_busy(date, tz, uid, email):
                     datetime.strptime(dt, '%Y%m%dT%H%M%S%z')
                     for dt in line.split(':')[1].split('/')
                 ]
-
             except IndexError:
                 continue
 
