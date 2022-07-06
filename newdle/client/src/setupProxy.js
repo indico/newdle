0167f1ee371a468cad3167f304a51bb65ad4573e
Adrian Moennich <adrian.moennich@cern.ch>
2022-07-06 17:53:29 +0200

    Fix build on newer node versions

--- newdle/client/src/setupProxy.js
+++ newdle/client/src/setupProxy.js
@@ -1,11 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 // we need a custom proxy config since for some reason the "proxy" option in package.json
 // forwards even the websocket requests that are meant for the auto reloader...
 // https://github.com/facebook/create-react-app/issues/7323
 module.exports = app => {
   const config = {target: process.env.FLASK_URL || 'http://127.0.0.1:5000'};
-  app.use(proxy('/api/', config));
-  app.use(proxy('/login/', config));
-  app.use(proxy('/logout/', config));
+  app.use('/api/', createProxyMiddleware(config));
+  app.use('/login/', createProxyMiddleware(config));
+  app.use('/logout/', createProxyMiddleware(config));
 };
