8d5673a0adfc5ac4fbb3e9b1da55789e3ce6a5b3
mic4ael <crossner90@gmail.com>
2022-10-04 11:32:32 +0000

    Prevent confusing changes to *.po files

--- newdle/client/.babel-plugin-macrosrc.js
+++ newdle/client/.babel-plugin-macrosrc.js
@@ -1,9 +1,14 @@
 const {execSync} = require('child_process');
 
-const urlMap = JSON.parse(execSync('flask url_map_to_json'));
+try {
+  const urlMap = JSON.parse(execSync('flask url_map_to_json'));
 
-module.exports = {
-  flaskURLs: {
-    urlMap,
-  },
-};
+  module.exports = {
+    flaskURLs: {
+      urlMap,
+    },
+  };
+} catch (ex) {
+  // exit with non-zero code to prevent confusing changes in *.po files
+  process.exit(ex.status);
+}
