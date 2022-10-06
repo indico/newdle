564a95373d3d48859c903c6681a81f4590d2a13e
mic4ael <email>
2022-10-06 11:17:07 +0200

    Avoid false zero exit code in babel macro

--- newdle/client/.babel-plugin-macrosrc.js
+++ newdle/client/.babel-plugin-macrosrc.js
@@ -9,6 +9,15 @@ try {
     },
   };
 } catch (ex) {
+  let exitCode = 1;
+
+  // ex.status is only present when the command passed to execSync fails, hence we need
+  // to make sure we don't pass undefined to process.exit since it is pretty much the same
+  // as process.exit(0)
+  if (ex.status !== undefined) {
+    exitCode = ex.status;
+  }
+
   // exit with non-zero code to prevent confusing changes in *.po files
-  process.exit(ex.status);
+  process.exit(exitCode);
 }
