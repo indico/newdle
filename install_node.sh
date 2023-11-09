e1b6a5eac055c1e110a18a47fa51319dbbf7f718
Tomas Roun <tomas.roun@cern.ch>
2023-11-09 17:16:39 +0100

    Use node 18 & stop using deprecated installer

---
+++ install_node.sh
@@ -0,0 +1,16 @@
+# https://github.com/nodesource/distributions#installation-instructions
+NODE_MAJOR=18
+
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key |
+    gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+# Create node deb repository
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" \
+    > /etc/apt/sources.list.d/nodesource.list
+
+# Install node and npm
+apt-get update
+apt-get install nodejs -y
