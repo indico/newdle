b9a090924b97d368f7a9c3f1fb08cd239aac781c
Adrian Moennich <adrian.moennich@cern.ch>
2025-07-04 11:59:34 +0200

    Fix generating comment about new DB revisions

--- ci/print_revision_sql.sh
+++ ci/print_revision_sql.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 # Get all the SQL queries from the first migration file to the last, in order
-# and print them with escaped newlines
+# and print them surrounded by a markdown comment for github
 set -e
-
 
 migration_file=$1
 latest_migration=$2
 
-if [ ! -z ${migration_file} ] && [ ! -z ${latest_migration} ]; then 
-    down_revision=$(grep -Po "(?<=^down_revision = ').*(?=')" $migration_file )
-    revision=$(grep -Po "(?<=^revision = ').*(?=')" $latest_migration )
+if [ ! -z ${migration_file} ] && [ ! -z ${latest_migration} ]; then
+    down_revision=$(grep -Po "(?<=^down_revision = ').*(?=')" $migration_file)
+    revision=$(grep -Po "(?<=^revision = ').*(?=')" $latest_migration)
+
     sql_command=$(flask db upgrade --sql ${down_revision}:${revision})
-    sql_command="${sql_command//'%'/'%25'}"
-    sql_command="${sql_command//$'\n'/'%0A'}"
-    sql_command="${sql_command//$'\r'/'%0D'}" 
-    echo $sql_command
-else 
+    [ -z "$sql_command" ] && exit 0
+
+    cat << EOF
+This PR contains database changes. Before merging it, make sure to apply the migration in production:
+
+\`\`\`sql
+${sql_command}
+\`\`\`
+
+When reviewing the PR, make sure that the changes will not break the previously deployed
+version, i.e. any new column needs to have a \`server_default\` or be nullable.
+EOF
+else
     exit 0
 fi
